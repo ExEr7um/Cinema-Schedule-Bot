8e6c9538c960a44f81811bc13db6f0c720ddc485
ExEr7um <exer7um@gmail.com>
2019-12-21 14:03:13 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -45,7 +45,7 @@ def today_handler(bot, update):
     html = BeautifulSoup(r.text, 'html.parser')
     [s.extract() for s in html('script')]
     tournaments = html.findAll("div", {"class": "seo-results__tournament"})
-    logger.info(html.find("div", {"class": "seo-results"}))
+    print(html.find("div", {"class": "seo-results"}))
     matches = html.find("div", {"class": "seo-results"}).findAll("ul")
     events = []
     for index, tournament in enumerate(tournaments):
