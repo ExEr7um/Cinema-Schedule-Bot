b942ff409c35a0c1500e22020b91ae29cf9b689b
ExEr7um <exer7um@gmail.com>
2019-12-21 22:38:51 +0300

    Сделал все виды спорта

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import os
 import sys
 import requests
 from bs4 import BeautifulSoup
-from datetime import datetime, date, time, timedelta
+from datetime import datetime, date, time
 
 from telegram.ext import Updater, CommandHandler
 
@@ -33,7 +33,7 @@ else:
 def start_handler(bot, update):
     logger.info("User {} started bot".format(update.effective_user["id"]))
     update.message.reply_text(
-        "Привет, {}!\r\nЯ умею показывать расписание спортивных событий".format(update.message.from_user.first_name))
+        "Привет, {}!\r\nЯ умею показывать расписание спортивных событий. Доступные виды спорта:\r\n/football — футбол\r\n/basketball — баскетбол\r\n/hockey — хоккей".format(update.message.from_user.first_name))
 
 
 def football_handler(bot, update):
@@ -55,7 +55,7 @@ def football_handler(bot, update):
                        "matches": tournament_matches})
     message = ""
     for event in events:
-        message += event.get("title")
+        message += "*" + event.get("title") + "*"
         for match in event.get("matches"):
             message += "\r\n{}. Начало: {}".format(
                 match.get("title"), match.get("time"))
@@ -63,7 +63,74 @@ def football_handler(bot, update):
     if len(message) > 4096:
         for x in range(0, len(message), 4096):
             bot.send_message(chat_id=update.message.chat_id,
-                             text=message[x:x+4096])
+                             text=message[x:x+4096],
+                             parse_mode=telegram.ParseMode.MARKDOWN)
+        else:
+            update.message.reply_text(message)
+
+
+def hockey_handler(bot, update):
+    today = str(datetime.today()).split()[0]
+    url = 'https://www.championat.com/stat/hockey/#' + today
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+    r = requests.get(url, headers=headers)
+    html = BeautifulSoup(r.text, 'html.parser')
+    tournaments = html.findAll("div", {"class": "seo-results__tournament"})
+    matches = html.find("div", {"class": "seo-results"}).findAll("ul")
+    events = []
+    for index, tournament in enumerate(tournaments):
+        tournament_matches = []
+        for match in matches[index].findAll("li"):
+            tournament_matches.append({"title": match.a.text, "time": match.find(
+                "span", {"class": "seo-results__item-date"}).text})
+        events.append({"title": tournament.a.text,
+                       "matches": tournament_matches})
+    message = ""
+    for event in events:
+        message += "*" + event.get("title") + "*"
+        for match in event.get("matches"):
+            message += "\r\n{}. Начало: {}".format(
+                match.get("title"), match.get("time"))
+        message += "\r\n\r\n"
+    if len(message) > 4096:
+        for x in range(0, len(message), 4096):
+            bot.send_message(chat_id=update.message.chat_id,
+                             text=message[x:x+4096],
+                             parse_mode=telegram.ParseMode.MARKDOWN)
+        else:
+            update.message.reply_text(message)
+
+
+def basketball_handler(bot, update):
+    today = str(datetime.today()).split()[0]
+    url = 'https://www.championat.com/stat/basketball/#' + today
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+    r = requests.get(url, headers=headers)
+    html = BeautifulSoup(r.text, 'html.parser')
+    tournaments = html.findAll("div", {"class": "seo-results__tournament"})
+    matches = html.find("div", {"class": "seo-results"}).findAll("ul")
+    events = []
+    for index, tournament in enumerate(tournaments):
+        tournament_matches = []
+        for match in matches[index].findAll("li"):
+            tournament_matches.append({"title": match.a.text, "time": match.find(
+                "span", {"class": "seo-results__item-date"}).text})
+        events.append({"title": tournament.a.text,
+                       "matches": tournament_matches})
+    message = ""
+    for event in events:
+        message += "*" + event.get("title") + "*"
+        for match in event.get("matches"):
+            message += "\r\n{}. Начало: {}".format(
+                match.get("title"), match.get("time"))
+        message += "\r\n\r\n"
+    if len(message) > 4096:
+        for x in range(0, len(message), 4096):
+            bot.send_message(chat_id=update.message.chat_id,
+                             text=message[x:x+4096],
+                             parse_mode=telegram.ParseMode.MARKDOWN)
         else:
             update.message.reply_text(message)
 
@@ -73,6 +140,11 @@ if __name__ == '__main__':
     updater = Updater(TOKEN)
 
     updater.dispatcher.add_handler(CommandHandler("start", start_handler))
-    updater.dispatcher.add_handler(CommandHandler("today", football_handler))
+    updater.dispatcher.add_handler(
+        CommandHandler("football", football_handler))
+    updater.dispatcher.add_handler(
+        CommandHandler("hockey", hockey_handler))
+    updater.dispatcher.add_handler(
+        CommandHandler("backetball", basketball_handler))
 
     run(updater)
