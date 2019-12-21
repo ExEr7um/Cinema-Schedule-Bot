ddb860c36fdeba77b838b4f9cc064ab0d54a6ab9
ExEr7um <exer7um@gmail.com>
2019-12-21 21:11:10 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -42,6 +42,8 @@ def today_handler(bot, update):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
     r = requests.get(url, headers=headers)
+    print(r)
+    print(r.text)
     html = BeautifulSoup(r.text, 'html.parser')
     [s.extract() for s in html('script')]
     print(html.find("body"))
